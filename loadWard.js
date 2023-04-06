7589e20ae570bd08c2169009099ff6748226c1c6
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-04-06 17:37:04 +0200

    Add button functionality + little goof

--- loadWard.js
+++ loadWard.js
@@ -1,3 +1,10 @@
 function loadWard(wardObject) {
-    wardObject
+    if (localStorage.getItem('playBgm')!==false) {
+        const bgm = new Audio('assets/music/track1.mp3')
+        bgm.volume = 0.25
+        bgm.loop = true
+        bgm.play()
+    }
 }
+
+export default loadWard
