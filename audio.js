09ac2dea5f03598a1c0019930d6e933cd98f8dd8
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-04-07 22:35:10 +0200

    fix

--- audio.js
+++ audio.js
@@ -1,10 +1,10 @@
-const PagerButton = new Audio('../assets/sounds/sndPagerButton.mp3')
+const PagerButton = new Audio('../../assets/sounds/sndPagerButton.mp3')
 PagerButton.volume = 0.25
 
-const SelectPatient = new Audio('../assets/sounds/sndSelectPatient.mp3')
+const SelectPatient = new Audio('../../assets/sounds/sndSelectPatient.mp3')
 SelectPatient.volume = 0.25
 
-const TransitionIn = new Audio('../assets/sounds/sndTransitionIn.mp3')
+const TransitionIn = new Audio('../../assets/sounds/sndTransitionIn.mp3')
 TransitionIn.volume = 0.5
 
 const RDAudio = { PagerButton, SelectPatient, TransitionIn }
--- loadWard.js
+++ loadWard.js
@@ -1,7 +1,7 @@
 import RDAudio from './audio.js'
 
 function loadWard(wardObject) {
-    if (localStorage.getItem('playBgm')!==false) {
+    if (localStorage.getItem('playBgm')!==true) {
         const bgm = new Audio('../../assets/music/track1.mp3')
         bgm.volume = 0.25
         bgm.loop = true
